24eca23dffcb83505d99e17badbb227bbc90747e
Iwona <iwonawintrowicz@gmail.com>
2023-07-03 23:58:44 +0200

    Separation of application code from html, adding templates including the base one and new routes including routes dynamic

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return '<h1>Witaj w świecie Flaska</h1>'
+    return render_template('index.html')
+
+@app.route('/kontakt')
+def kontakt():
+    return render_template('kontakt.html')
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', the_name=name)
 
 if __name__ == "__main__":
     app.run()
