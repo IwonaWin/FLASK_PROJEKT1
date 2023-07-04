95015c1419173deceb0acefd0f65f9b3513ec30b
Iwona <iwonawintrowicz@gmail.com>
2023-07-04 14:04:10 +0200

    adding the first database and supplementing with data entered manually by the terminal

--- app.py
+++ app.py
@@ -5,15 +5,28 @@ from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
 
 class NameForm(FlaskForm):
     name = StringField('Jakie jest Twoje imię?', validators=[DataRequired()])
     submit = SubmitField('Wyślij')
+    
+class User(db.Model): #Class which represents all people in school
+    id = db.Column(db.Integer, primary_key=True, unique=True) #unique key
+    firstname = db.Column(db.String(30)) #firstname user
+    lastname = db.Column(db.String(30)) #lastnameuser
+    role = db.Column(db.String(15)) #role uf user: director/student/teacher
+    group = db.Column(db.String(5)) #name of group students
+    subject = db.Column(db.String(20)) #for teacher, subjecht which it's learning
+    supervising = db.Column(db.String(5)) #if teacher have own group add here name of this group
+    active = db.Column(db.Boolean) #if user is login or logout on the page
+    
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -34,6 +47,12 @@ def kontakt():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', the_name=name)
+
+@app.route('/people')
+def people():
+    db.create_all()
+    uzytkownicy = User.query.all()   
+    return render_template('people.html', the_uzytkownicy=uzytkownicy)
 
 @app.errorhandler(404)
 def page_not_found(e):
