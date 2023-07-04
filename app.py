83149957226f04ee2780334c4d7cec633d741b68
Iwona <iwonawintrowicz@gmail.com>
2023-07-04 12:10:44 +0200

    Configuring the config file, adding 1 class to the app and adding 1 simple form

--- app.py
+++ app.py
@@ -2,14 +2,27 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from datetime import datetime
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config.from_pyfile('config.cfg')
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('Jakie jest Twoje imię?', validators=[DataRequired()])
+    submit = SubmitField('Wyślij')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',the_current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html',the_current_time=datetime.utcnow(), the_form=form,the_name=name)
 
 @app.route('/kontakt')
 def kontakt():
